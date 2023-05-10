19dc90260757ddf549edd1471a59dd4ff0f2fbcf
Blake Frangoulis <blakef44@gmail.com>
2023-05-10 21:43:52 +0000

    updated categorizeAcidity

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -57,7 +57,7 @@ function categorizeAcidity(pH) {
     return 'neutral';
   } else if (pH >= 0 && pH < 7) {
     return 'acid';
-  } else if (pH > 7 && pH < 14) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else {
     return 'invalid pH level';
