75d133a1695d1890b0d3ee8bb8a000b954ca46ae
Blake Frangoulis <blakef44@gmail.com>
2023-05-26 18:31:28 +0000

    completed javascript-and-json

---
+++ javascript-and-json/main.js
@@ -0,0 +1,28 @@
+const library = [
+  {
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    isbn: '9780061122415',
+  },
+  {
+    title: 'The Kite Runner',
+    author: 'Khaled Hosseini',
+    isbn: '9781594631931',
+  },
+  {
+    title: 'The Plague',
+    author: 'Albert Camus',
+    isbn: '9780679720218',
+  },
+];
+
+console.log('library:', library);
+console.log('typeof library:', typeof library);
+
+const stringifyLibrary = JSON.stringify(library);
+console.log('stringifyLibrary:', stringifyLibrary);
+console.log('typeof stringifyLibrary:', typeof stringifyLibrary);
+
+const parsedString = JSON.parse(stringifyLibrary);
+console.log('parsedString:', parsedString);
+console.log('typeof parsedString:', typeof parsedString);
