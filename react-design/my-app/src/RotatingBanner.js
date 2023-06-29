39204f5e38b969af4b93c28370b65d9662365aab
Blake Frangoulis <blakef44@gmail.com>
2023-06-29 21:26:09 +0000

    moved definitions of sub-components outside of RotatingBanner component definition. Adjusted Indicators component to no longer use items.map

--- react-design/my-app/src/RotatingBanner.js
+++ react-design/my-app/src/RotatingBanner.js
@@ -2,44 +2,41 @@ import { useState } from 'react';
 
 function RotatingBanner({ items }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  function Banner({ item, currentIndex }) {
-    return (
-      <div>
-        <h1>{item}</h1>
-      </div>
-    );
-  }
-  function PrevButton() {
-    return (
-      <div>
-        <button>Prev</button>
-      </div>
-    );
-  }
-  function Indicators({ count, current }) {
-    return (
-      <div>
-        {items.map((item, index) => (
-          <button>{index + 1}</button>
-        ))}
-      </div>
-    );
-  }
-  function NextButton() {
-    return (
-      <div>
-        <button>Next</button>
-      </div>
-    );
-  }
   return (
     <div>
       <Banner item={items[currentIndex]} />
       <PrevButton />
-      <Indicators count={items.length} current={currentIndex} />
+      <Indicators count={items.length} />
       <NextButton />
     </div>
   );
 }
-
+function Banner({ item, currentIndex }) {
+  return (
+    <div>
+      <h1>{item}</h1>
+    </div>
+  );
+}
+function PrevButton() {
+  return (
+    <div>
+      <button>Prev</button>
+    </div>
+  );
+}
+function Indicators({ count, currentIndex }) {
+  const buttonsArray = [];
+  for (let i = 0; i < count; i++) {
+    buttonsArray.push(<button>{i}</button>);
+  }
+  return <div>{buttonsArray}</div>;
+}
+function NextButton() {
+  return (
+    <div>
+      <button>Next</button>
+    </div>
+  );
+}
 export default RotatingBanner;
