863ea4e7c20ff500cedbfa02cac2f07746d7ef10
Blake Frangoulis <blakef44@gmail.com>
2023-07-06 17:55:57 +0000

    mostly finished node module system

---
+++ node-module-system/calculate.js
@@ -0,0 +1,26 @@
+// import  add from "./add.js";
+// import subtract from "./subtract.js";
+// import multiply from "./multiply.js";
+// import divide from "./divide.js";
+
+// function calculate(arg1, operator, arg2) {
+//   switch (operator) {
+//     case 'minus':
+//       return subtract(arg1, arg2);
+//     case 'plus':
+//       return add(arg1, arg2);
+//     case 'times':
+//       return multiply(arg1, arg2);
+//     case 'over':
+//       return divide(arg1, arg2);
+//   }
+//   return undefined;
+// }
+
+const arg1 = Number(process.argv[2]);
+// const operator = process.argv[3];
+// const arg2 = Number(process.argv[4]);
+
+if (isNaN(arg1)) {
+  console.log('invalid argument:', process.argv[2]);
+}
