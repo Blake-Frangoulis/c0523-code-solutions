3ab30e54bc4a8565dd49755475fafe10f6d2f3da
Blake Frangoulis <blakef44@gmail.com>
2023-07-27 03:54:39 +0000

    completed express-post-json

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-// const nextId = 1;
+let nextId = 1;
 const grades = {};
 
 app.use(express.json());
@@ -13,4 +13,20 @@ app.get('/api/grades', (req, res) => {
     gradesArray.push(grades[id]);
   }
   res.json(gradesArray);
+});
+
+app.post('/api/grades', (req, res) => {
+  const newGrade = req.body;
+  if (!newGrade.name || !newGrade.course || !newGrade.score) {
+    res.status(400).send('Incomplete Request');
+    return;
+  }
+  const id = nextId++;
+  newGrade.id = id;
+  grades[id] = newGrade;
+  res.status(201).json(newGrade);
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
 });
